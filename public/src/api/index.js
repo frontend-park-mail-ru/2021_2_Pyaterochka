7cd98bfa4b5341ed2d9df676f581e516c4285899
Volodya Larin <volodyalarin@bk.ru>
2021-10-31 23:27:12 +0300

    change server to proxy

--- public/src/api/index.js
+++ public/src/api/index.js
@@ -1,7 +1,7 @@
 /** @module API */
 
-// const basename = 'https://secure-proxy.api.pyaterochka-team.site/api/v1';
-const basename = 'http://api.pyaterochka-team.site:8080/api/v1';
+const basename = 'https://secure-proxy.api.pyaterochka-team.site/api/v1';
+//const basename = 'http://api.pyaterochka-team.site:8080/api/v1';
 
 const mapCreator = (data) => {
     return {
